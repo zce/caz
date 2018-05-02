f09d435a5c379e560c6021393694fa05cd96d624
zce <w@zce.me>
2018-05-02 17:07:07 +0800

    :arrow_up: Upgrade deps

--- src/pages/board/board.js
+++ src/pages/board/board.js
@@ -10,61 +10,31 @@ Page({
     boards: [
       { key: 'in_theaters' },
       { key: 'coming_soon' },
-      { key: 'top250' }
+      { key: 'new_movies' },
+      { key: 'top250' },
       // { key: 'weekly' },
-      // { key: 'new_movies' },
-      // { key: 'us_box', name: '北美票房榜' }
-    ],
-    loading: true
+      // { key: 'us_box' }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad () {
-    const promises = this.data.boards.map(board => {
-      return app.douban.find(board.key, 1, 10)
+    wx.showLoading({ title: '拼命加载中...' })
+
+    const tasks = this.data.boards.map(board => {
+      return app.douban.find(board.key, 1, 8)
         .then(d => {
           board.title = d.title
           board.movies = d.subjects
           return board
         })
     })
-    Promise.all(promises).then(boards => this.setData({ boards: boards, loading: false }))
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady () {
-    // TODO: onReady
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow () {
-    // TODO: onShow
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide () {
-    // TODO: onHide
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload () {
-    // TODO: onUnload
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh () {
-    // TODO: onPullDownRefresh
+    Promise.all(tasks).then(boards => {
+      this.setData({ boards: boards, loading: false })
+      wx.hideLoading()
+    })
   }
 })
