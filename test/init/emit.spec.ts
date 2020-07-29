e7c314470b6e22de3b6c09cf59eeb3e8dcf610f8
zce <w@zce.me>
2020-07-29 11:12:40 +0800

    test: init emit test

--- test/init/emit.spec.ts
+++ test/init/emit.spec.ts
@@ -1,5 +1,24 @@
+import fs from 'fs'
+import path from 'path'
+import { createContext } from './util'
 import emit from '../../src/init/emit'
 
 test('unit:init:emit', async () => {
   expect(typeof emit).toBe('function')
+})
+
+test('unit:init:emit:all', async () => {
+  const ctx = createContext({
+    dest: path.join(__dirname, '../.temp'),
+    files: [
+      { path: 'hello.txt', contents: Buffer.from('hello') },
+      { path: 'foo.txt', contents: Buffer.from('foo') }
+    ]
+  })
+  await emit(ctx)
+  const hello = await fs.promises.readFile(path.join(ctx.dest, 'hello.txt'), 'utf8')
+  expect(hello).toBe('hello')
+  const foo = await fs.promises.readFile(path.join(ctx.dest, 'foo.txt'), 'utf8')
+  expect(foo).toBe('foo')
+  await fs.promises.rmdir(ctx.dest, { recursive: true })
 })
