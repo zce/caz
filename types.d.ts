ea816cb97148bab1f4e0cb50f1f33b901900a58b
zce <w@zce.me>
2021-07-26 18:59:39 +0800

    chore: meta tweaks

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,7 @@
 /**
  * PackageJson declaration type.
  * for import package.js as ESM without having to participate in compilation
+ * declarations.d.ts
  */
 declare module '*/package.json' {
   export const name: string
